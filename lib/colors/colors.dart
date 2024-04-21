5405becbdcd5f78327ecbe751507482eb3a6010d
hilmanmutaqin <hilman3384@gmail.com>
2024-04-21 19:39:23 +0700

    Add - bottom navigation

---
+++ lib/colors/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color navigation = const Color(0xFFFF4D02);
+Color textNavigation = const Color(0xFFF7F3E7);
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,24 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter/colors/colors.dart';
+import 'package:latihan_flutter/screen/info_screen.dart';
+import 'add_screen.dart';
+import 'profile_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const Center(child: Text('Home Screen')),
+    const AddScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Latihan Flutter App'),
-        backgroundColor: Colors.blue[600],
-        actions: [IconButton(
-          onPressed: () {}, 
-          icon: const Icon(Icons.info))],
+        foregroundColor: textNavigation,
+        backgroundColor: navigation,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const InfoScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.info))
+        ],
       ),
-      body: const Center(child: Text('Sample')),
-      drawer: const Drawer(),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-      },child: const Icon(Icons.add),
+      body: _screens[_selectedIndex],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.home, color: navigation),
+              title: const Center(child: Text("Home")),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.add, color: navigation),
+              title: const Center(child: Text("Add")),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.people, color: navigation),
+              title: const Center(child: Text("Profile")),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProfileScreen(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      
+      endDrawer: const Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: textNavigation,
+        unselectedItemColor: Colors.black,
+        selectedLabelStyle: TextStyle(color: textNavigation),
+        unselectedLabelStyle: const TextStyle(color: Colors.black),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Profile',
+          ),
+        ],
+        backgroundColor: navigation,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
