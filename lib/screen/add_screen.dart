aa8778d37231f428fc61ee0c33ee19d73df61f27
hilmanmutaqin <hilman3384@gmail.com>
2024-04-22 20:48:58 +0700

    add - dialog

--- lib/screen/add_screen.dart
+++ lib/screen/add_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter/colors/colors.dart';
 
 class AddScreen extends StatelessWidget {
   const AddScreen({super.key});
@@ -6,10 +7,58 @@ class AddScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: const Center(
-        child: Text('Add Screen'),
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(bottom: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Masukkan teks di sini',
+                  labelText: 'Teks',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Konfirmasi'),
+                      content: Text('Apakah Anda yakin ingin menambahkan?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("tidak"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("iya"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text(
+                'Tampilkan Dialog',
+                style: TextStyle(color: textNavigation),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: navigation,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -69,7 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const AddScreen(),
+                    builder: (context) =>  const AddScreen(),
                   ),
                 );
               },
