683e378edb7cd973eb3e1f65879f21f8a7b2563c
hilmanmutaqin <hilman3384@gmail.com>
2024-05-22 20:00:31 +0700

    fix - detail screen

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -16,7 +16,12 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
-class DigitalCard extends StatelessWidget {
+class DigitalCard extends StatefulWidget {
+  @override
+  _DigitalCardState createState() => _DigitalCardState();
+}
+
+class _DigitalCardState extends State<DigitalCard> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -38,9 +43,9 @@ class DigitalCard extends StatelessWidget {
       mainAxisSpacing: 20,
       crossAxisSpacing: 20,
       children: [
-        _buildCardItem("Hilman", "Mahasiswa", "0821xxxxxx", "hilman***@gmail.com", 'images/user2.png'),
-        _buildCardItem("fery", "Designer", "0812xxxxxx", "fery***@gmail.com", 'images/user2.png'),
-        _buildCardItem("vito", "Developer", "0856xxxxxx", "vito***@gmail.com", 'images/user2.png'),
+        _buildCardItem("Hilman", "Mahasiswa", "0821xxxxxx", "hilman***@gmail.com", 'https://img.icons8.com/?size=100&id=13042&format=png&color=000000'),
+        _buildCardItem("Ananda", "Designer", "0812xxxxxx", "Ananda***@gmail.com", 'https://img.icons8.com/?size=100&id=nuMODtuVLj4A&format=png&color=000000'),
+        _buildCardItem("Raveendra", "Developer", "0856xxxxxx", "Ravee***@gmail.com", 'https://img.icons8.com/?size=100&id=108639&format=png&color=000000'),
       ],
     );
   }
@@ -49,60 +54,83 @@ class DigitalCard extends StatelessWidget {
     return ListView(
       padding: EdgeInsets.all(20),
       children: [
-        _buildCardItem("Hilman", "Mahasiswa", "0821xxxxxx", "hilman***@gmail.com", 'images/user2.png'),
+        _buildCardItem("Hilman", "Mahasiswa", "0821xxxxxx", "hilman***@gmail.com", 'https://img.icons8.com/?size=100&id=13042&format=png&color=000000'),
         SizedBox(height: 20),
-        _buildCardItem("fery", "Designer", "0812xxxxxx", "fery***@gmail.com", 'images/user1.png'),
+        _buildCardItem("Ananda", "Designer", "0812xxxxxx", "Ananda***@gmail.com", 'https://img.icons8.com/?size=100&id=nuMODtuVLj4A&format=png&color=000000'),
         SizedBox(height: 20),
-        _buildCardItem("vito", "Developer", "0856xxxxxx", "vito***@gmail.com", 'images/user3.png'),
+        _buildCardItem("Raveendra", "Developer", "0856xxxxxx", "Ravee***@gmail.com", 'https://img.icons8.com/?size=100&id=108639&format=png&color=000000'),
       ],
     );
   }
 
-  Widget _buildCardItem(String name, String pekerjaan, String noHP, String email, String imagePath) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3),
+  Widget _buildCardItem(String name, String pekerjaan, String noHP, String email, String imageUrl) {
+    bool isHovered = false;
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return MouseRegion(
+          onEnter: (event) => setState(() => isHovered = true),
+          onExit: (event) => setState(() => isHovered = false),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: isHovered ? Colors.blue.shade50 : Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+                SizedBox(height: 20),
+                Flexible(
+                  child: Text(
+                    name,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Flexible(
+                  child: Text(
+                    pekerjaan,
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Flexible(
+                  child: Text(
+                    noHP,
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Flexible(
+                  child: Text(
+                    email,
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage(imagePath),
-          ),
-          SizedBox(height: 20),
-          Text(
-            name,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            pekerjaan,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 10),
-          Text(
-            noHP,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 10),
-          Text(
-            email,
-            style: TextStyle(fontSize: 16),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
